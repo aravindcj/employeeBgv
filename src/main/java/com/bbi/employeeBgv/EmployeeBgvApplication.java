cd26963056740ec740f7e7fb0a82952451eb86f2
aravindcj <aravindj239@gmail.com>
2025-02-14 14:42:51 +0530

    Initial commit

---
+++ src/main/java/com/bbi/employeeBgv/EmployeeBgvApplication.java
@@ -0,0 +1,13 @@
+package com.bbi.employeeBgv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBgvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBgvApplication.class, args);
+	}
+
+}
